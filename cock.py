7f3770af33b2cc5ea351a2f2ffb736cf7e5096da
gaius_stonks <33606512+codingnerd267@users.noreply.github.com>
2020-11-09 15:04:38 -0800

    Update cock.py

--- cock.py
+++ cock.py
@@ -1,22 +1,21 @@
 from random import randint
-gaiusSucks = 0
-zekeSucks = 0
 
 def suckies(password):
     if password==69420:
+        gaiusSucks = 0
+        zekeSucks = 0
         for i in range(5):
             random = randint (0, 10)
             gaiusSucks += random
             random = randint (0, 10)
             zekeSucks += random
         if gaiusSucks > zekeSucks:
-            return "Gaius sucks more"
+            print ("Gaius sucks more")
         elif zekeSucks > gaiusSucks:
-            return "Zeke sucks more"
+            print ("Zeke sucks more")
         else:
-            return "Both suck eachother"
+            print ("Both suck eachother")
 
-        
+
 suckies(69420)
-
         
