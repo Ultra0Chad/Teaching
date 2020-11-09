cd3d91edc2dc729e75040c9292c9c5997a39a591
gaius_stonks <33606512+codingnerd267@users.noreply.github.com>
2020-11-09 13:06:02 -0800

    Update cock.py

--- cock.py
+++ cock.py
@@ -1,1 +1,26 @@
-print ("poopie")
+from random import randint
+
+def suckies(password):
+    if password==69420:
+        whoSucks= []
+        for i in range(5):
+            j = randint(0,2)
+            if j==0:
+                whoSucks.append("gaiusSucks")
+            if j==1:
+                whoSucks.append("zekeSucks")
+        zekeSucks = 0
+        gaiusSucks = 0
+        for i in whoSucks:
+            if i == "gaiusSucks":
+                gaiusSucks += 1
+            if i == "zekeSucks":
+                zekeSucks += 1
+        if zekeSucks >= 3:
+            print("zeke sucks more")
+        elif gaiusSucks >= 3:
+            print("gaius sucks more")
+        else:
+            print ("neither")
+
+suckies(69420)
