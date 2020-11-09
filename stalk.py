1af392b54855501583ece7193b770f688d82ff45
Ultra0Chad <zekescott00@gmail.com>
2020-11-09 10:46:14 -0800

    fixing boobs

--- stalk.py
+++ stalk.py
@@ -1,17 +1,8 @@
+print("run stalkSetup.py before the first time running this, only need to run it once")
 import instagram_explore as ie 
-# import instaloader
-# import requests
-# from datetime import datetime
-# import json 
 from instabot import Bot
-# import os
-# import time as time
 import urllib
 image = urllib.request.URLopener()
-
-
-# using the tag method 
-# L = instaloader.Instaloader()
 
 bot = Bot()
 
@@ -21,22 +12,10 @@ result = ie.tag(boobTag)
 bot.login(username = "ventilgamingofficial",  
           password = "gotitbro64") 
 
-# result.data['edge_hashtag_to_media']['edges'][0]['node'].keys()
-
-sexy = result.data['edge_hashtag_to_top_posts']['edges'][0]['node']['thumbnail_src']
-
-print(result.data['edge_hashtag_to_top_posts']['edges'][0]['node']['thumbnail_src'])
-
-# def getImage(url):
-#     response = requests.get(url)
-#     file = open("sample.jpg", "wb")
-#     file.write(response.content)
-#     file.close()
 
 boobCount = input("How many boobs?")
 
 for i in range(int(boobCount)):
-    # getImage(sexy)
     try:
         sexy = result.data['edge_hashtag_to_top_posts']['edges'][i]['node']['thumbnail_src']
         imgName = (str(i) + ".jpg")
